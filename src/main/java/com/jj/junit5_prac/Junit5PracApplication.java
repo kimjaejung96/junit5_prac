cd107b929682a51015cbbad00bd0a9a94a7cdb99
kimjaejung96 <jj.kim@okestro.com>
2023-01-27 17:43:39 +0900

    [Source Generate]

---
+++ src/main/java/com/jj/junit5_prac/Junit5PracApplication.java
@@ -0,0 +1,13 @@
+package com.jj.junit5_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5PracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5PracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jj/junit5_prac/Junit5PracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jj.junit5_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5PracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
